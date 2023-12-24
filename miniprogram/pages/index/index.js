9e7aeac3f46880b33f2e76e89f13968bb6c74bed
ZWF <2360690156@qq.com>
2023-12-24 19:42:07 +0800

    实现创建学期弹层基本功能

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,10 +1,11 @@
 // pages/index/index.js
+// import Toast from '@vant/weapp/toast/toast';
 
 const semester = {
   2023: ['春','秋'],
-  2022: ['春','秋'],
-  2021: ['春','秋'],
-  2020: ['春','秋'],
+  2022: ['春1','秋'],
+  2021: ['春2','秋'],
+  2020: ['春3','秋'],
 };
 
 Page({
@@ -14,6 +15,12 @@ Page({
    */
   data: {
     isTeacher:true,
+    semester: {
+      2023: ['春','秋'],
+      2022: ['春1','秋'],
+      2021: ['春2','秋'],
+      2020: ['春3','秋'],
+    },
     showSemester:false,
     semesterColumns: [
       {
@@ -26,6 +33,7 @@ Page({
         defaultIndex: 0,
       },
     ],
+    semesterKeyValue:[],
     isLoading:false
   },
   triggerIdentity(){
@@ -33,17 +41,37 @@ Page({
       isTeacher:!this.data.isTeacher
     })
   },
+  // 显示弹出层同时关闭TabBar
   createSemester() {
     wx.hideTabBar()
     this.setData({ showSemester: true });
   },
+  // 控制弹出层的关闭同时显示TabBar
   onCloseSemester() {
     this.setData({ showSemester: false });
     wx.showTabBar()
+    // console.log('关闭');
   },
+  // 用户修改弹出层内学期
   onChangeSemester(event) {
     const { picker, value, index } = event.detail;
+    // console.log(picker);
+    // console.log(value);
+    // console.log(index);
+    // 随第一列的更新，第二列更新得到对应数据
     picker.setColumnValues(1, semester[value[0]]);
+  },
+  // picker取消按钮
+  onCancelSemester() {
+    this.onCloseSemester()
+  },
+  // picker确认按钮
+  onConfirmSemester(event) {
+    const { picker, value, index } = event.detail;
+    this.setData({
+      semesterKeyValue:value
+    })
+    this.onCloseSemester()
   },
   /**
    * 生命周期函数--监听页面加载
