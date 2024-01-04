fd45b7a68efe9218d020cc727cb59d49c15a5ec9
ZWF <2360690156@qq.com>
2024-01-04 16:30:29 +0800

    完成签到班级+学期的选择数据存储

--- miniprogram/pages/features/features.js
+++ miniprogram/pages/features/features.js
@@ -8,10 +8,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    semester:{},
+    course:{},
     showCourseChoice: false,
-    actions: [
+    showSemesterChoice: false,
+    courseActions: [
       {
         name: '选项',
+        courseId:1
+      },
+      {
+        name: '选项',
+      },
+      {
+        name: '选项',
+      },
+    ],
+    semesterActions: [
+      {
+        name: '选项',
+        courseId:1
       },
       {
         name: '选项',
@@ -28,11 +44,36 @@ Page({
     })
     wx.hideTabBar()
   },
+  onShowSemesterChoice() {
+    // console.log(1);
+    this.setData({
+      showSemesterChoice:true
+    })
+    wx.hideTabBar()
+  },
   onCloseCourseChoice() {
     this.setData({
       showCourseChoice:false
     })
     wx.showTabBar()
+  },
+  onCloseSemesterChoice() {
+    this.setData({
+      showSemesterChoice:false
+    })
+    wx.showTabBar()
+  },
+  onSelectCourse(e) {
+    // console.log(e.detail);
+    this.setData({
+      course:e.detail
+    })
+  },
+  onSelectSemester(e) {
+    // console.log(e.detail);
+    this.setData({
+      semester:e.detail
+    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -52,8 +93,30 @@ Page({
       title: '签到',
     })
     // console.log(this.data.semesterAndCourseData);
-    this.data.semesterAndCourseData.map((semester,index)=>{
-      console.log(semester.semesterName);
+    const tempSemester = this.data.semesterAndCourseData.map((semester,index)=>{
+      // console.log(semester.semesterName);
+      return {
+        semesterId: semester._id,
+        name: semester.semesterName 
+      }
+    })
+    this.setData({
+      semesterActions:tempSemester
+    })
+    let tempCourse = []
+    for(let i=0;i<this.data.semesterAndCourseData.length;i++) {
+      const tempObj = this.data.semesterAndCourseData[i].courses.map((course,index)=>{
+        // console.log(semester.semesterName);
+        return {
+          courseId: course.courseId,
+          name: course.courseName 
+        }
+      })
+      // console.log(tempObj);
+      tempCourse = [...tempCourse,tempObj[0]]
+    }
+    this.setData({
+      courseActions:tempCourse
     })
   },
 
--- miniprogram/pages/rolechoice/rolechoice.js
+++ miniprogram/pages/rolechoice/rolechoice.js
@@ -49,7 +49,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    wx.setNavigationBarTitle({
+      title:'角色选择'
+    })
   },
 
   /**
